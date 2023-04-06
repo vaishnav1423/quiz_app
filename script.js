830f0f15f77407aa43a5ea158a14fc350e6ddc7c
Vaishnav Kedar <vaishnav1423@gmail.com>
2023-04-06 09:24:08 +0000

    Added some touch

--- script.js
+++ script.js
@@ -60,6 +60,7 @@ function startQuiz() {
   showQuestion();
 }
 function showQuestion() {
+  resetState();
   let currentQuestion = questions[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
@@ -69,6 +70,54 @@ function showQuestion() {
     button.innerHTML = answer.text;
     button.classList.add("btn");
     answerButton.appendChild(button);
+    if (answer.correct){
+      button.dataset.correct=answer.correct;
+    }
+    button.addEventListener("click",selectAnswer);
   });
 }
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
+function selectAnswer(e){
+  const selectedButton=e.target;
+  const isCorrect=selectedButton.dataset.correct==="true";
+  if (isCorrect){
+    selectedButton.classList.add("correct");
+    score++;
+  }else{
+    selectedButton.classList.add("incorrect");
+  }
+  Array.from(answerButton.children).forEach(button=>{
+    if(button.dataset.correct==="true"){
+      button.classList.add("correct");
+    }
+    button.disabled=true;
+  });
+  nextButton.style.display="block";
+}
+function showScore(){
+  resetState();
+  questionElement.innerHTML=`You have scored ${score} out of ${questions.length}!`
+  nextButton.innerHTML="Play Again !!";
+  nextButton.style.display="block";
+}
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex<questions.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
+}
+nextButton.addEventListener("click",()=>{
+  if (currentQuestionIndex<questions.length){
+    handleNextButton();
+  }else{
+    startQuiz();
+  }
+});
 startQuiz();
